1fc1a4ead3e4284a8dd8a543d563b98f46f9553e
Carlos-CodeBot <jossephrguezz@gmail.com>
2022-08-13 08:07:52 -0500

    1.3

---
+++ Punto 1/index.js
@@ -0,0 +1,28 @@
+function add_li() {
+    var entrada = document.getElementById("entrada").value.toLowerCase();
+
+    if (entrada.length > 0) {
+        var li = document.createElement('li');
+        li.id = entrada;
+        var text = document.createTextNode(entrada);
+        li.appendChild(text);
+        document.getElementById("lista").appendChild(li);
+
+
+    }
+    document.getElementById("entrada").value = "";
+    return false
+
+
+}
+
+function eliminar() {
+    let ele = document.querySelectorAll('li');
+    let cant = ele.length;
+    if (cant > 0) {
+        ele = ele[0];
+        ele.remove();
+    }
+
+    return false;
+}
